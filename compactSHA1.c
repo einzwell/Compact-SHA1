8ccefdeb4454940b8053e64c0e8b2631f34f7979
Einzwell <git@einzwell.dev>
2024-04-08 14:17:12 +0700

    Rename variables to use snake_case

--- compactSHA1.c
+++ compactSHA1.c
@@ -17,74 +17,75 @@
  * @brief Calculates the SHA-1 hash of given input.
  *
  * @param msg The input buffer.
- * @param mLen Size of the input data (excluding null terminator).
+ * @param msg_len Size of the input data (excluding null terminator).
  * @param digest The output buffer. Must be at least 20 bytes.
  *
  * @return 0 on success, otherwise non-zero.
  */
 
-int compactSHA1(const uint8_t *restrict msg, const uint64_t mLen, uint8_t *restrict digest) {
-	/* Sanity check */
-	if (!msg || !digest) return -1;
+int compactSHA1(const uint8_t *restrict msg, const uint64_t msg_len, uint8_t *restrict digest) {
+    /* Sanity check */
+    if (!msg || !digest)
+        return -1;
 
 	/* Calculate the total length of message plus padding */
 	uint64_t pmLen = mLen + (64 - (mLen % 56) + ((mLen < 56) ? 0 : 8));
 
-	/* Initialise the word schedule, the padded message, the IVs, and the constants */
-	uint32_t word[80];
-	uint8_t padMsg[pmLen];
-	uint32_t h[5] = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
-	const uint32_t k[4] = {0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6};
+    /* Initialise the word schedule, the padded message, the IVs, and the constants */
+    uint32_t word[80];
+    uint8_t pad_msg[pad_msg_len];
+    uint32_t h[5] = {0x67452301, 0xEFCDAB89, 0x98BADCFE, 0x10325476, 0xC3D2E1F0};
+    const uint32_t k[4] = {0x5A827999, 0x6ED9EBA1, 0x8F1BBCDC, 0xCA62C1D6};
 
-	/* Begin the padding process */
-	memset(padMsg, 0x00, pmLen);
-	memcpy(padMsg, msg, mLen);
-	padMsg[mLen] = 0x80;
-	for (size_t pmIdx = 8; pmIdx > 0; pmIdx--) {
-		padMsg[pmLen - pmIdx] = (uint8_t)((mLen * 8) >> (pmIdx * 8 - 8));
-	}
+    /* Begin the padding process */
+    memset(pad_msg, 0x00, pad_msg_len);
+    memcpy(pad_msg, msg, msg_len);
+    pad_msg[msg_len] = 0x80;
+    for (size_t pad_msg_i = 8; pad_msg_i > 0; pad_msg_i--) {
+        pad_msg[pad_msg_len - pad_msg_i] = (uint8_t) ((msg_len * 8) >> (pad_msg_i * 8 - 8));
+    }
 
-	/* Divide the padded message into 512-bit chunks */
-	for (size_t cIdx = 0; cIdx < pmLen / 64; cIdx++) {
-		/* Prepare the message schedule */
-		for (size_t wIdx = 0; wIdx < 16; wIdx++) {
-			word[wIdx] = (padMsg[cIdx * 64 + wIdx * 4 + 0] << 24) +
-						 (padMsg[cIdx * 64 + wIdx * 4 + 1] << 16) +
-						 (padMsg[cIdx * 64 + wIdx * 4 + 2] << 8) +
-						 (padMsg[cIdx * 64 + wIdx * 4 + 3] << 0);
-		}
+    /* Divide the padded message into 512-bit chunks */
+    for (size_t c_i = 0; c_i < pad_msg_len / 64; c_i++) {
+        /* Prepare the message schedule */
+        for (size_t w_i = 0; w_i < 16; w_i++) {
+            word[w_i] = (pad_msg[c_i * 64 + w_i * 4 + 0] << 24) +
+                        (pad_msg[c_i * 64 + w_i * 4 + 1] << 16) +
+                        (pad_msg[c_i * 64 + w_i * 4 + 2] << 8) +
+                        (pad_msg[c_i * 64 + w_i * 4 + 3]);
+        }
 
-		for (size_t wIdx = 16; wIdx < 80; wIdx++) {
-			word[wIdx] = word[wIdx - 3] ^ word[wIdx - 8] ^ word[wIdx - 14] ^ word[wIdx - 16];
-			word[wIdx] = (word[wIdx] << 1) | (word[wIdx] >> 31);
-		}
-		
-		/* Initialise the working variables with the intermediate hash value */
-		uint32_t a = h[0], b = h[1], c = h[2], d = h[3], e = h[4], t, f;
+        for (size_t w_i = 16; w_i < 80; w_i++) {
+            word[w_i] = word[w_i - 3] ^ word[w_i - 8] ^ word[w_i - 14] ^ word[w_i - 16];
+            word[w_i] = (word[w_i] << 1) | (word[w_i] >> 31);
+        }
 
-		/* Begin the compression function */
-		for (size_t wIdx = 0; wIdx < 80; wIdx++) {
-			f = (wIdx / 20 == 0) ? d ^ (b & (c ^ d)) : (wIdx / 20 == 1 || wIdx / 20 == 3) ? b ^ c ^ d : (b & c) | (d & (b | c));
-			t = ((a << 5) | (a >> 27)) + f + e + k[wIdx / 20] + word[wIdx];
-			e = d;
-			d = c;
-			c = (b << 30) | (b >> 2);
-			b = a;
-			a = t;
-		}
-		
-		/* Compute the new intermediate hash value */
-		h[0] += a;
-		h[1] += b;
-		h[2] += c;
-		h[3] += d;
-		h[4] += e;
-	}
+        /* Initialise the working variables with the intermediate hash value */
+        uint32_t a = h[0], b = h[1], c = h[2], d = h[3], e = h[4], t, f;
 
-	/* Copy the resultant hash into the digest buffer (big-endian) */
-	for (size_t dIdx = 0; dIdx < 20; dIdx++) {
-		digest[dIdx] = (uint8_t)(h[dIdx / 4] >> (24 - ((dIdx % 4) * 8)));
-	}
+        /* Begin the compression function */
+        for (size_t w_i = 0; w_i < 80; w_i++) {
+            f = (w_i / 20 == 0) ? d ^ (b & (c ^ d)) : (w_i / 20 == 1 || w_i / 20 == 3) ? b ^ c ^ d : (b & c) | (d & (b | c));
+            t = ((a << 5) | (a >> 27)) + f + e + k[w_i / 20] + word[w_i];
+            e = d;
+            d = c;
+            c = (b << 30) | (b >> 2);
+            b = a;
+            a = t;
+        }
 
-	return 0;
+        /* Compute the new intermediate hash value */
+        h[0] += a;
+        h[1] += b;
+        h[2] += c;
+        h[3] += d;
+        h[4] += e;
+    }
+
+    /* Copy the resultant hash into the digest buffer (big-endian) */
+    for (size_t d_i = 0; d_i < 20; d_i++) {
+        digest[d_i] = (uint8_t) (h[d_i / 4] >> (24 - ((d_i % 4) * 8)));
+    }
+
+    return 0;
 }
